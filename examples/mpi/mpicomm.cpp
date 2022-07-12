0c545f84978f86448fdd0b1a1a886f2948c255a8
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2022-07-12 10:44:25 +0800

    Update to comm example

--- examples/mpi/mpicomm.cpp
+++ examples/mpi/mpicomm.cpp
@@ -13,11 +13,13 @@
 #include <omp.h>
 #endif
 
+
 #include <mpi.h>
 
 // if want to try running code but not do any actual communication
 // #define TURNOFFMPI
 
+int Niter=1;
 int ThisTask, NProcs;
 
 #define LogMPITest() if (ThisTask==0) std::cout<<" running "<<mpifunc<< " test"<<std::endl;
@@ -50,17 +52,12 @@ std::tuple<int,
     std::vector<std::string> mpi_comms_name(numcoms);
     std::vector<int> ThisLocalTask(numcoms), NProcsLocal(numcoms), NLocalComms(numcoms);
 
-    mpi_comms[0] = MPI_COMM_WORLD;
-    ThisLocalTask[0] = ThisTask;
-    NProcsLocal[0] = NProcs;
-    NLocalComms[0] = 1;
-    mpi_comms_name[0] = "world";
-    for (auto i=1;i<=numcomsold;i++) 
+    for (auto i=0;i<=numcomsold;i++) 
     {
         NLocalComms[i] = NProcs/pow(2,i);
         if (NLocalComms[i] < 2) 
         {
-            numcoms = i-1;
+            numcoms = i+1;
             break;
         }
         auto ThisLocalCommFlag = ThisTask % NLocalComms[i];
@@ -69,16 +66,21 @@ std::tuple<int,
         MPI_Comm_rank(mpi_comms[i], &ThisLocalTask[i]);
         MPI_Comm_size(mpi_comms[i], &NProcsLocal[i]);
     }
+    mpi_comms[numcoms-1] = MPI_COMM_WORLD;
+    ThisLocalTask[numcoms-1] = ThisTask;
+    NProcsLocal[numcoms-1] = NProcs;
+    NLocalComms[numcoms-1] = 1;
+    mpi_comms_name[numcoms-1] = "world";
     ThisLocalTask.resize(numcoms);
     NLocalComms.resize(numcoms);
     NProcsLocal.resize(numcoms);
     mpi_comms.resize(numcoms);
     mpi_comms_name.resize(numcoms);
-    for (auto i=0;i<=numcoms;i++) 
+    for (auto i=0;i<numcoms;i++) 
     {
         if (ThisTask==0) std::cout<<" MPI communicator "<<mpi_comms_name[i]<<" has size of "<<NProcsLocal[i]<<" and there are "<<NLocalComms[i]<<" communicators"<<std::endl;
     }
-    MPI_Barrier(mpi_comms[0]);
+    MPI_Barrier(mpi_comms[numcoms-1]);
     return std::make_tuple(numcoms,
         std::move(mpi_comms),
         std::move(mpi_comms_name), 
@@ -87,8 +89,9 @@ std::tuple<int,
         std::move(NLocalComms)
         );
 }
+
 void MPIFreeComms(std::vector<MPI_Comm> &mpi_comms, std::vector<std::string> &mpi_comms_name){
-    for (auto i=1;i<=mpi_comms.size();i++) {
+    for (auto i=0;i<mpi_comms.size()-1;i++) {
         if (ThisTask==0) std::cout<<"Freeing "<<mpi_comms_name[i]<<std::endl;
         MPI_Comm_free(&mpi_comms[i]);
     }
@@ -105,24 +108,48 @@ std::vector<unsigned long long> MPISetSize(double maxgb)
     return sizeofsends;
 }
 
-void MPIReportTimeStats(profiling_util::Timer time1, std::string f, std::string l)
+std::vector<float> MPIGatherTimeStats(profiling_util::Timer time1, std::string f, std::string l)
 {
     std::vector<float> times(NProcs);
     auto p = times.data();
     auto time_taken = profiling_util::GetTimeTaken(time1, f, l);
     MPI_Gather(&time_taken, 1, MPI_FLOAT, p, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    return times;
+}
+
+std::tuple<float, float, float, float> TimeStats(std::vector<float> times) 
+{
+    auto ave = 0.0, std = 0.0;
+    auto mint = times[0];
+    auto maxt = times[0];
+    for (auto &t:times)
+    {
+        ave += t;
+        std += t*t;
+        mint = std::min(mint,t);
+        maxt = std::max(maxt,t);
+    }
+    float n = times.size();
+    ave /= n;
+    if (n>1) std = sqrt((std - ave*ave*n)/(n-1.0));
+    else std = 0;
+    return std::make_tuple(ave, std, mint, maxt);
+}
+
+void MPIReportTimeStats(profiling_util::Timer time1, std::string f, std::string l)
+{
+    auto times = MPIGatherTimeStats(time1, f, l);
     if (ThisTask == 0) {
-        auto ave = 0.0, std = 0.0;
-        for (auto &t:times) 
-        {
-            ave += t;
-            std += t*t;
-        }
-        ave /= static_cast<float>(NProcs);
-        std = sqrt((std - ave*ave*NProcs)/(NProcs-1.0));
-        std::cout<<f<<" time taken stats is "<<ave<<" +/- "<<std<<std::endl;
+        auto[ave, std, mint, maxt] = TimeStats(times);
+        std::cout<<f<<"@"<<l<<": time taken stats is "<<ave<<" +/- "<<std<<" with [min,max]=["<<mint<<","<<maxt<<"]"<<std::endl;
     }
     MPI_Barrier(MPI_COMM_WORLD);
+}
+
+void ReportTimeStats(std::vector<float> times, std::string f, std::string l)
+{
+    auto[ave, std, mint, maxt] = TimeStats(times);
+    std::cout<<f<<"@"<<l<<": time taken stats is "<<ave<<" +/- "<<std<<" with [min,max]=["<<mint<<","<<maxt<<"]"<<std::endl;
 }
 
 
@@ -201,26 +228,21 @@ void MPITestAllReduce(Options &opt)
         p1 = data.data();
         p2 = allreducesum.data();
         auto time1 = NewTimer();
-        for (auto j=1;j<=mpi_comms.size();j++) 
+        for (auto j=0;j<mpi_comms.size();j++) 
         {
-            auto time2 = NewTimer();
+#ifdef TURNOFFMPI
+#else
             if (ThisLocalTask[j] == 0) std::cout<<ThisTask<<" / "<<ThisLocalTask[j]<<" : Communicating using comm "<<mpi_comms_name[j]<<std::endl;
-#ifdef TURNOFFMPI
-#else 
-            MPI_Allreduce(p1, p2, sizeofsends[i], MPI_DOUBLE, MPI_SUM, mpi_comms[j]);
+            std::vector<float> times;
+            for (auto iter=0;iter<Niter;iter++) {
+                auto time2 = NewTimer();
+                MPI_Allreduce(p1, p2, sizeofsends[i], MPI_DOUBLE, MPI_SUM, mpi_comms[j]);
+                auto times_tmp = MPIGatherTimeStats(time2, __func__, std::to_string(__LINE__));
+                times.insert(times.end(), times_tmp.begin(), times_tmp.end());
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+            if (ThisTask == 0) ReportTimeStats(times, __func__, std::to_string(__LINE__));
 #endif
-            MPIReportTimeStats(time2, __func__, std::to_string(__LINE__));
-        }
-        {
-            auto time2 = NewTimer();
-            if (ThisLocalTask[0] == 0) std::cout<<ThisTask<<" / "<<ThisLocalTask[0]<<" : Communicating using comm "<<mpi_comms_name[0]<<std::endl;
-#ifdef TURNOFFMPI
-            std::this_thread::sleep_for(std::chrono::seconds(10));
-            for (auto &d:allreducesum) d = pow(2.0,ThisTask);
-#else 
-            MPI_Allreduce(p1, p2, sizeofsends[i], MPI_DOUBLE, MPI_SUM, mpi_comms[0]);
-#endif
-            MPIReportTimeStats(time2, __func__, std::to_string(__LINE__));
         }
         if (ThisTask==0) LogTimeTaken(time1);
     }
@@ -250,7 +272,8 @@ int main(int argc, char **argv) {
     auto start = std::chrono::system_clock::now();
     std::time_t start_time = std::chrono::system_clock::to_time_t(start);
     if (ThisTask==0) std::cout << "Starting job at " << std::ctime(&start_time);
-    if (argc == 2) opt.maxgb = atof(argv[1]);
+    if (argc >= 2) opt.maxgb = atof(argv[1]);
+    if (argc == 3) Niter = atof(argv[2]);
     
     if (ThisTask==0) LogParallelAPI();
     MPI_Barrier(MPI_COMM_WORLD);
