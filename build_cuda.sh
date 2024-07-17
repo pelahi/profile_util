0dba9081d119f7b9fc1f1c0b39c135e362f9027f
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2024-07-17 16:41:44 +0800

    update to build cuda to use c++17

--- build_cuda.sh
+++ build_cuda.sh
@@ -9,8 +9,6 @@ if [ ! -z $2 ]; then
     MPICXX=$2
 fi
 
-#MPIFLAGS=$(mpic++ --showme | sed "s:nvcc ::g" | sed "s:-Wl,-rpath:-Xcompiler -Wl,-rpath:g" | sed "s:-Wl,:-Xcompiler -Wl,:g" )
-
 devicetype=cuda
 OMPFLAGS=-mp
 # first is serial
@@ -18,13 +16,13 @@ buildtypes=("CUDA Serial" "CUDA OpenMP" "CUDA MPI" "CUDA MPI+OpenMP")
 buildnames=("_cuda" "_cuda_omp" "_cuda_mpi" "_cuda_mpi_omp")
 compilers=(${CXX} ${CXX} "${MPICXX}" "${MPICXX}")
 extraflags=("-D_CUDA" "-D_CUDA ${OMPFLAGS}" "-D_CUDA -D_MPI ${MPIFLAGS}" "-D_CUDA -D_MPI ${MPIFLAGS} ${OMPFLAGS}")
-CXXFLAGS="-Xcompiler -fPIC -Xcompiler -std=c++17 -O2"
+CXXFLAGS="-Xcompiler -fPIC -Xcompiler -std=c++17 -O3 -cuda"
 
 
 for ((i=0;i<4;i++)) 
 do 
     echo "BUILDNAME=${buildnames[$i]} BUILDNAME=${buildnames[$i]} DEVICETYPE=${devicetype}"
     make BUILDNAME=${buildnames[$i]} BUILDNAME=${buildnames[$i]} DEVICETYPE=${devicetype} clean
-    make CXXFLAGS="${CXXFLAGS}" BUILDNAME=${buildnames[$i]} BUILDNAME=${buildnames[$i]} DEVICETYPE=${devicetype} CXX="${compilers[$i]}" COMPILER="${compilers[$i]}" EXTRAFLAGS="${extraflags[$i]}"  -j
+    make CXXFLAGS="${CXXFLAGS}" BUILDNAME=${buildnames[$i]} BUILDNAME=${buildnames[$i]} DEVICETYPE=${devicetype} CXX="${compilers[$i]}" COMPILER="${compilers[$i]}" EXTRAFLAGS="${extraflags[$i]}" -j
 done
 
