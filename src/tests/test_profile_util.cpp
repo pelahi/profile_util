8b11eb24f8712ce75233521c8c3abbd2001f07ae
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2024-09-11 16:53:46 +0800

    Update to test, added stuff for GPUs

--- src/tests/test_profile_util.cpp
+++ src/tests/test_profile_util.cpp
@@ -2,17 +2,16 @@
 #include <iostream>
 #include <numeric>
 #include <vector>
+#include <random>
 #include <profile_util.h>
 
 int main(int argc, char *argv[])
 {
-    int NProcs = 1;
     int ThisTask = 0;
 #ifdef _MPI
     auto comm = MPI_COMM_WORLD;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(comm, &ThisTask);
-    MPI_Comm_size(comm, &NProcs);
     MPISetLoggingComm(comm);
 #endif 
 #ifdef _MPI
@@ -26,6 +25,37 @@ int main(int argc, char *argv[])
     LogSystemMem();
 #endif
     LogMemUsage();
+    auto t1 = NewTimerHostOnly();
+    auto s1 = NewSampler(0.01);
+    sleep(1);
+    std::vector<float> xvec(1000000);
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(1.0,2.0);
+    for (auto &x:xvec) 
+    {
+        x = distribution(generator);
+        x = exp(-x*x)*x/(1.0+x)+sin(pow(x*x,0.25));
+    }
+    LogCPUUsage(s1);  
+    LogTimeTaken(t1);
+
+#ifdef _GPU
+    auto Nentries = xvec.size();
+    int nDevices;
+    std::vector<float*> xdev;
+    pu_gpuErrorCheck(pu_gpuGetDeviceCount(&nDevices));
+    xdev.resize(nDevices);
+    auto tgpumem = NewTimerHostOnly();
+    for (auto idev=0;idev<nDevices;idev++) {
+        pu_gpuErrorCheck(pu_gpuSetDevice(idev));
+        auto time_local = NewTimer();
+        auto nbytes = Nentries * sizeof(float);
+        pu_gpuErrorCheck(pu_gpuMalloc(&xdev[idev], nbytes));
+        pu_gpuErrorCheck(pu_gpuDeviceSynchronize());
+        LogTimeTakenOnDevice(time_local);
+    }
+    LogTimeTaken(tgpumem);
+#endif
 
 #ifdef _MPI
     MPI_Finalize();
